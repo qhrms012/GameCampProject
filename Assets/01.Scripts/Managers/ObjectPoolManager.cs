09e1db279c5dc031465150447a0fe2cae4dab665
boguen <qhrms012@naver.com>
2025-08-15 16:31:31 +0900

    [add] 제네릭싱글톤 및 오브젝트풀

--- Assets/01.Scripts/Managers/ObjectPoolManager.cs
+++ Assets/01.Scripts/Managers/ObjectPoolManager.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class ObjectPoolManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] preFabs;
+
+
+    List<GameObject>[] pools;
+
+    private void Awake()
     {
-        
+        pools = new List<GameObject>[preFabs.Length];
+
+        for (int index = 0; index < pools.Length; index++)
+        {
+            pools[index] = new List<GameObject>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject Get(int index)
     {
-        
+        GameObject select = null;
+
+        foreach (GameObject item in pools[index])
+        {
+            if (!item.activeSelf)
+            {
+                select = item;
+                select.SetActive(true);
+                break;
+            }
+        }
+
+        if (!select)
+        {
+            select = Instantiate(preFabs[index], transform);
+            pools[index].Add(select);
+        }
+
+        return select;
     }
+
 }
